a4f2b890a0b7bc5b8bc3e9a9d0187c036d355ccd
Z00One <9000248@g.yju.ac.kr>
2024-03-25 10:19:43 +0900

    feat: Add ScheduleModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-import { BoardsModule } from './boards/boards.module';
-import { CommentsModule } from './comments/comments.module';
-import { ReplysModule } from './replys/replys.module';
-import { LikeModule } from './like/like.module';
-import { TagsModule } from './tags/tags.module';
-import { BoardTagsModule } from './board-tags/board-tags.module';
-import { UploadModule } from './upload/upload.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
+import { AuthModule } from "./auth/auth.module";
+import { UserModule } from "./user/user.module";
+import { BoardsModule } from "./boards/boards.module";
+import { CommentsModule } from "./comments/comments.module";
+import { ReplysModule } from "./replys/replys.module";
+import { LikeModule } from "./like/like.module";
+import { TagsModule } from "./tags/tags.module";
+import { BoardTagsModule } from "./board-tags/board-tags.module";
+import { UploadModule } from "./upload/upload.module";
+import { ScheduleModule } from "@nestjs/schedule";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env',
+      envFilePath: ".env",
       isGlobal: true,
     }),
     AuthModule,
@@ -27,6 +28,7 @@ import { UploadModule } from './upload/upload.module';
     TagsModule,
     BoardTagsModule,
     UploadModule,
+    ScheduleModule.forRoot(),
   ],
   controllers: [AppController],
   providers: [AppService],
